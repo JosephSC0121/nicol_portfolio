ea2e38f4d0dcaf475a105200959f49a746701b10
JosephSC0121 <josephsilvacasas0121@gmail.com>
2025-03-29 22:08:37 -0500

    feat update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'server',
+  output: "server",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   adapter: vercel(),
 });
